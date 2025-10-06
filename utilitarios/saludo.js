76124a15aa432dff82cebaadfdea26a665122083
andyQuezada10 <quezadaandy08@gmail.com>
2025-10-06 17:23:00 -0500

    se saco funciones a otro archivo js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -9,35 +9,3 @@ saludar=function(){
     mostrarTextoEnCaja("txtNombre"," ");
 }
 
-mostrarTextoEnCaja=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.value=mensaje; 
-}
-cambiarImagen=function(idComponente,imagen){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.src=imagen;
-}
-recuperarTexto=function(idComponente){
-    let cmpTexto;
-    let valor;
-    cmpTexto=document.getElementById(idComponente);
-    valor=cmpTexto.value;
-    return valor;
-}
-recuperarInt=function(idComponente){
-    let valor=recuperarTexto(idComponente);
-    let valorEntero=parseInt(valor);
-    return valorEntero;
-}
-recuperarFloat=function(idComponente){
-    let valor=recuperarTexto(idComponente);
-    let valorFloat=parseFloat(valor);
-    return valorFloat;
-}
-mostrarTexto=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.innerText=mensaje;
-}
