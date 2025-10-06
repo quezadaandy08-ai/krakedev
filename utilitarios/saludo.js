e3f15415d38270be172ca8c87eefd4127d4f5009
andyQuezada10 <quezadaandy08@gmail.com>
2025-10-06 12:01:39 -0500

    se añadio Recuperar INT y FLOAT

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar=function(){
     let Nombre=recuperarTexto("txtNombre");
     let Apellido=recuperarTexto("txtApellido");
+    let Edad=recuperarInt("txtEdad");
+    let Estatura=("txtEstatura");
 }
 
 recuperarTexto=function(idComponente){
@@ -9,4 +11,14 @@ recuperarTexto=function(idComponente){
     cmpTexto=document.getElementById(idComponente);
     valor=cmpTexto.value;
     return valor;
+}
+recuperarInt=function(idComponente){
+    let valor=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valor);
+    return valorEntero;
+}
+recuperarFloat=function(idComponente){
+    let valor=recuperarTexto(idComponente);
+    let valorFloat=parseFloat(valor);
+    return valorFloat;
 }
