1ab605cd2575ebc1cb9af183b7dc7c6f93d0dc46
andyQuezada10 <quezadaandy08@gmail.com>
2025-10-06 17:52:11 -0500

    se agrego el valor descuento

--- 2-puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
+++ 2-puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
@@ -35,7 +35,9 @@ calcularValorTotal = function () {
         Si el caso de prueba es exitoso, hacer un commit
      */
     //6. Invocar a calcularValorDescuento y lo que devuelve guardar en la variable valorDescuento
+     valorDescuento=calcularValorDescuento(valorSubtotal, porcentajeDescuento);
     //7. Mostrar el resultado en el componente lblDescuento
+    mostrarTexto("lblDescuento",valorDescuento);
     /*
         Caso de prueba: 
             - cantidad: 10 
