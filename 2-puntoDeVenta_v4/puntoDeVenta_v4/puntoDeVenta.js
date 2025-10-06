9afa11d98ae6de552b7d7845102784c6a7b45931
andyQuezada10 <quezadaandy08@gmail.com>
2025-10-06 17:57:40 -0500

    se agrego el valor total

--- 2-puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
+++ 2-puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
@@ -67,7 +67,9 @@ calcularValorTotal = function () {
             Si el caso de prueba es exitoso, hacer un commit
         */
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+    valorTotal = calcularTotal(valorSubtotal, valorDescuento, valorIVA)
     //11. Mostrar el resultado en el componente lblTotal
+    mostrarTexto("lblTotal",valorTotal);
     /*
         Caso de prueba: 
             - cantidad: 10
@@ -84,6 +86,7 @@ calcularValorTotal = function () {
        */
             
     //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
+    
     /*
         Ejemplo: 
             Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
