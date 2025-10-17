7514d6beaa47cea556034eed5f99f853cfe18e6c
andyQuezada10 <quezadaandy08@gmail.com>
2025-10-17 10:36:43 -0500

    se ajusto el js acorde lo que me pide

--- ImagenDadosArchivos/ImagenDadosArchivos/imagenDados.js
+++ ImagenDadosArchivos/ImagenDadosArchivos/imagenDados.js
@@ -1,71 +1,67 @@
-let puntos;
- puntos=0;
- let lanzamientos;
- lanzamientos=5;
-
+let puntos = 0;
+let lanzamientos = 5;
 
 jugar=function(){
     let resultado;
     resultado=lanzarDado();
     console.log(resultado);
-    mostarCara(resultado);
+   mostrarCara(resultado);
     modificarPuntos(resultado);
-    modificarLanzamientos();
+    modificarLanzamientos(resultado);
     if(puntos > 20){
-        mostrarMensaje("Ganaste","ganaste");
+        mostrarMensaje("GANASTE!!!","ganaste");
         limpiar();
     }
- 
 }
 
 modificarPuntos=function(numero){
-  puntos=puntos+numero;
-  cambiarTexto("lblPuntos",puntos);
-  //obtener mas de 20 mostar mensaje ganaste
-  //invocar a limpiar
+    puntos=puntos+numero;
+    cambiarTexto("lblPuntos",puntos);
 }
+
 modificarLanzamientos=function(){
     lanzamientos=lanzamientos-1;
-    cambiarTexto("lblLanzamientos", lanzamientos);
-   //el puntaje llega a cero mostrar game over
-   if(lanzamientos == 0){
+    cambiarTexto("lblLanzamientos",lanzamientos);
+    if(lanzamientos == 0){
             if(puntos <= 20){
-            mostrarMensaje("Game Over","perdiste");
+            mostrarMensaje("GAME OVER","perdiste");
             limpiar()
-            }
-            }
-}
-limpiar=function(){
-    puntos=0;
-    lanzamientos=5;
-    cambiarTexto("lblPuntos", puntos);
-    cambiarTexto("lblLanzamientos", lanzamientos);
-    cambiarImagen("imgDado", "");
-
-}
-mostrarMensaje=function(texto, clase){
-    let mensaje = document.getElementById("lblMensaje");
-    mensaje.innerText = texto;
-    mensaje.className = "win" + clase;
-}
-
-mostarCara=function(numero){
-    if(numero==1){
-        cambiarImagen("imagenDados","dados1.png");
-    }else if(numero==2){
-        cambiarImagen("imagenDados","dados2.png");
-    }else if(numero==3){
-        cambiarImagen("imagenDados","dados3.png");
-    }else if(numero==4){
-        cambiarImagen("imagenDados","dados4.png");
-    }else if(numero==5){
-        cambiarImagen("imagenDados","dados5.png");
-    }else if(numero==6){
-        cambiarImagen("imagenDados","dados6.png");
+        }
     }
 }
 
+limpiar=function(){
+    puntos = 0;
+    lanzamientos = 5;
+    cambiarTexto("lblPuntos",puntos);
+    cambiarTexto("lblLanzamientos",lanzamientos);
+    cambiarImagen("imgDado","")
+}
 
+mostrarMensaje=function(texto, clase){
+    let mensaje = document.getElementById("lblMensaje");
+    mensaje.innerText = texto;
+    mensaje.className = "win " + clase;
+}
+
+//Función mostrarCara, recibe el numero que quiere mostrar
+//Muestra la imagen correspondiente al numero que recibe
+//No retorna nada
+mostrarCara=function(numero){
+    if(numero == 1){ //Con == se compara, mientras que con = se asigna
+        cambiarImagen("imgDado","dados1.png");
+    }else if(numero == 2){
+         cambiarImagen("imgDado","dados2.png");
+    }else if(numero == 3){
+         cambiarImagen("imgDado","dados3.png");
+    }else if(numero == 4){
+         cambiarImagen("imgDado","dados4.png");
+    }else if(numero == 5){
+         cambiarImagen("imgDado","dados5.png");
+    }else if(numero == 6){
+         cambiarImagen("imgDado","dados6.png");
+    }
+}
 
 lanzarDado=function(){
     let aleatorio;
